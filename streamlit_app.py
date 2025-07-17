a842108032a100bab4cb74dc0de0ea948b5f0643
scottjkozlowski <kozlos@yahoo.com>
2025-07-17 13:04:49 -0400

    Update streamlit_app.py
    change my.smoothiefroot.com to fuityvice.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ if ingredients_list:
 
 # New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
